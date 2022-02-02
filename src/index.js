6b876b8fac216a96bb7ddeb16618630bf294fec6
Igor Gladkiy <egunka1988@gmail.com>
2022-02-02 14:42:00 +0200

    add visual and logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './sass/main.scss';
+
+const form = document.querySelector('.form')
+const backdrop = document.querySelector('.backdrop')
+const modal = document.querySelector('.modal')
+
+function getParams(e) {
+    e.preventDefault()
+
+    const obj = {
+        height: +form.elements.height.value,
+        width: +form.elements.width.value,
+        isAmalgama: form.elements.isAmalgama.checked,
+        lightType: form.elements.lightType.value,
+        mkv: +(form.elements.height.value/1000) * +(form.elements.width.value/1000),
+        mpog: (+(form.elements.height.value/1000) + +(form.elements.width.value/1000))*2
+    }
+
+    openModal(obj)
+}
+
+const openModal = (obj) => {
+    backdrop.classList.remove('hidden')
+    modal.innerHTML = ''
+    
+    const markup = `
+        <p class="modal__field">Высота ${obj.height}</p>
+        <p class="modal__field">Ширина ${obj.width}</p>
+        <p class="modal__field">Свечение ${obj.lightType}</p>
+        <p class="modal__field">м.кв ${obj.mkv}</p>
+        <p class="modal__field">м.пог ${obj.mpog}</p>
+    `
+
+    modal.insertAdjacentHTML('afterbegin', markup)
+}
+
+const closeModal = () => {
+    backdrop.classList.add('hidden')
+}
+
+form.addEventListener('submit', getParams)
+backdrop.addEventListener('click', closeModal)
